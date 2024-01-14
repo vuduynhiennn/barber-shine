529d81a32b8372f6762af120899ccf9a687ed01b
nhienduyvu <nhienduyvu@gmail.com>
2024-01-14 11:13:34 +0700

    feat: header ui

---
+++ barber-shine-fe/src/components/landing-page/header/MENU.conficts.ts
@@ -0,0 +1,22 @@
+export const HEADER_MENUS = [
+  {
+    label: "Trang chủ",
+    href: "/",
+  },
+  {
+    label: "Về Zuka salon",
+    href: "/about",
+  },
+  {
+    label: "Địa chỉ Zuka salon",
+    href: "/",
+  },
+  {
+    label: "Học cắt tóc",
+    href: "/",
+  },
+  {
+    label: "Hạng thành viên",
+    href: "/",
+  },
+];
--- barber-shine-fe/src/components/landing-page/header/index.tsx
+++ barber-shine-fe/src/components/landing-page/header/index.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { HEADER_MENUS } from "./MENU.conficts";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className="flex h-14 py-1.5 justify-between items-center">
+      <div>ZUKA SALON</div>
+      <ul className="flex items-center gap-7.5">
+        {HEADER_MENUS.map((el) => (
+          <li key={el.href}>
+            <Link className="text-dark-300 font-semibold" href={el.href}>
+              {el.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <Button
+        variant="outline"
+        className="text-primary px-3 rounded-lg border-primary hover:bg-primary hover:bg-light h-6 py-0.5"
+      >
+        Đăng nhập
+      </Button>
+    </div>
+  );
 }
