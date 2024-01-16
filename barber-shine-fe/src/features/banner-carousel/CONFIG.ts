74a2097493ec99147aec181dcd3a9da4ebcd2ad4
nhienduyvu <nhienduyvu@gmail.com>
2024-01-17 01:49:43 +0700

    feat: banner carousel

---
+++ barber-shine-fe/src/features/banner-carousel/CONFIG.ts
@@ -0,0 +1,27 @@
+import Banner1 from '@/assets/images/banner1.jpg'
+import Banner2 from '@/assets/images/banner2.jpg'
+import Banner3 from '@/assets/images/banner3.jpg'
+import Banner1Mb from '@/assets/images/banner1_mb.jpeg'
+import Banner2Mb from '@/assets/images/banner2_mb.jpg'
+import Banner3Mb from '@/assets/images/banner3_mb.jpg'
+
+export const BANNERS_CONFIG = [
+  {
+    _id: '1',
+    alt: 'banner1',
+    pc: Banner1,
+    mb: Banner1Mb,
+  },
+  {
+    _id: '2',
+    alt: 'banner2',
+    pc: Banner2,
+    mb: Banner2Mb,
+  },
+  {
+    _id: '3',
+    alt: 'banner3',
+    pc: Banner3,
+    mb: Banner3Mb,
+  },
+]
