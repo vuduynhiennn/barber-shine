2bdadd921b22b66d57e948ae6bef42dc66e352b2
nhienduyvu <nhienduyvu@gmail.com>
2024-01-17 15:02:39 +0700

    chore: temp page mold

--- barber-shine-fe/src/pages/address.tsx
+++ barber-shine-fe/src/pages/address.tsx
@@ -1,19 +1,18 @@
 import DevelopingWarn from '@/components/ui/developing'
-import { WEB_NAME } from '@/constant/web-name'
 import LandingLayout from '@/layout/LandingLayout'
 import Wrapper from '@/layout/Wrapper'
 import React, { ReactElement } from 'react'
 
-export default function AboutPage() {
+export default function AddressPage() {
   return (
     <div>
-      Giới thiệu {WEB_NAME}
+      Địa chỉ
       <DevelopingWarn />
     </div>
   )
 }
 
-AboutPage.getLayout = function getLayout(page: ReactElement) {
+AddressPage.getLayout = function getLayout(page: ReactElement) {
   return (
     <LandingLayout>
       <Wrapper className="md:py-3.5">{page}</Wrapper>
