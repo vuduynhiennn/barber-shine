b575156ec8878ef36c6541d1c1012936a1360396
nhienduyvu <nhienduyvu@gmail.com>
2024-01-17 14:17:53 +0700

    chore: menu config

--- barber-shine-fe/src/components/dashboard/sidebar/menu.config.tsx
+++ barber-shine-fe/src/components/dashboard/sidebar/menu.config.tsx
@@ -1,13 +1,6 @@
-import IconSetup from 'lmss_share_libs/icons/IconSetup.svg'
-import UserVneckHair from '~/icons/fontawesome/regular/user-vneck-hair.svg'
-import MoneyCheckDollarPen from '~/icons/fontawesome/regular/money-check-dollar-pen.svg'
-import CartShoppingIcon from '~/icons/fontawesome/regular/cart-shopping.svg'
-import ClockRotateLeft from '~/icons/fontawesome/regular/clock-rotate-left.svg'
-import QrCode from '~/icons/fontawesome/regular/qrcode.svg'
-import HouseWindow from '~/icons/fontawesome/regular/house-window.svg'
-import BagShopping from '~/icons/fontawesome/regular/badge-check.svg'
-import UserLockIcon from '~/icons/fontawesome/regular/user-lock.svg'
-import ImageIcon from '~/icons/fontawesome/regular/images.svg'
+import HouseWindow from '@/icons/house-window-regular.svg'
+import UserLockIcon from '@/icons/user-lock-regular.svg'
+import ImageIcon from '@/icons/user-lock-regular.svg'
 import { ButtonSideBarProps } from './ButtonSidebar'
 import { UserRoleEnum } from '@/types/user.enum'
 
@@ -32,132 +25,6 @@ export const MENUS: MenuItem[] = [
     href: '/admin-dashboard/banner',
   },
   {
-    role_require: [UserRoleEnum.SIM_DATA_MANAGER],
-    key: 'product-manager',
-    label: 'Sản Phẩm',
-    icon: <BagShopping />,
-    sub_menus: [
-      {
-        key: 'add-new-sim',
-        label: 'Thêm',
-        href: '/admin-dashboard/sim/add',
-      },
-      {
-        key: 'list-sim',
-        label: 'Sim Data',
-        href: '/admin-dashboard/sim',
-      },
-      {
-        key: 'list-sim-otp',
-        label: 'Sim OTP',
-        href: '/admin-dashboard/sim/otp',
-      },
-      {
-        key: 'list-sim-promotion',
-        label: 'Sim Khuyến Mãi',
-        href: '/admin-dashboard/sim/promotion',
-      },
-      {
-        key: 'list-sim-beauty',
-        label: 'Sim Số Đẹp',
-        href: '/admin-dashboard/sim/beauty',
-      },
-    ],
-  },
-  {
-    role_require: [UserRoleEnum.ORDER_MANAGER],
-
-    key: 'order-manager',
-    href: `/admin-dashboard/orders`,
-    label: `Đơn Hàng`,
-    icon: <CartShoppingIcon />,
-  },
-  {
-    role_require: [UserRoleEnum.PAYMENT_VIEW],
-
-    key: 'recharge-manager',
-    label: `Nạp Tiền`,
-    icon: <MoneyCheckDollarPen />,
-    sub_menus: [
-      {
-        key: 'recharge',
-        label: 'Cộng Tiền',
-        href: `/admin-dashboard/payment/recharge`,
-        role_require: [UserRoleEnum.PAYMENT_ADD],
-      },
-      {
-        key: 'history-recharge',
-        label: 'Lịch Sử',
-        href: `/admin-dashboard/payment`,
-      },
-      {
-        key: 'revenue-report',
-        label: 'Doanh Thu',
-        href: `/admin-dashboard/payment/reports`,
-      },
-    ],
-  },
-  {
-    role_require: [UserRoleEnum.ROLE_MANAGER],
-    key: 'user-manager',
-    href: '/admin-dashboard/user',
-    label: 'User',
-    icon: <UserVneckHair />,
-  },
-  {
-    role_require: [UserRoleEnum.VNMB_SETUP],
-    key: 'agency-support',
-    href: '/admin-dashboard/hoitrodaily',
-    label: 'Setup VNMB',
-    icon: <IconSetup />,
-  },
-  {
-    key: 'service-manager',
-    label: 'Dịch Vụ',
-    icon: <QrCode />,
-    role_require: [UserRoleEnum.ORDER_MANAGER],
-    sub_menus: [
-      {
-        key: '5kvnmb',
-        label: 'Nạp 5k,10k... VNMB',
-        href: `/admin-dashboard/services/recharge`,
-      },
-      {
-        key: 'checksdt',
-        label: 'Check Sđt',
-        href: `/admin-dashboard/services/check-sim`,
-      },
-    ],
-  },
-  {
-    label: 'Dịch Vụ',
-    key: 'service',
-    icon: <QrCode />,
-    href: `/user/services`,
-    hide_if_admin: true,
-  },
-  {
-    key: 'order',
-    href: `/user/orders`,
-    label: `Đơn Hàng`,
-    icon: <CartShoppingIcon />,
-    hide_if_admin: true,
-  },
-  {
-    key: 'recharge-history',
-    href: '/payment/history',
-    label: 'Lịch Sử Nạp Tiền',
-    icon: <ClockRotateLeft />,
-    hide_if_admin: true,
-  },
-  {
-    key: 'recharge',
-    href: '/user/recharge',
-    label: 'Nạp tiền',
-    icon: <MoneyCheckDollarPen />,
-    hide_if_admin: true,
-  },
-  {
     key: 'user-profile',
     label: 'Tài Khoản',
     icon: <UserLockIcon />,
@@ -165,18 +32,18 @@ export const MENUS: MenuItem[] = [
       {
         key: 'user-secure',
         label: 'Đổi Mật Khẩu',
-        href: `/user/setting/change-password`,
+        href: `/setting/change-password`,
       },
       {
         key: 'login-history',
         label: 'Lich Sử Đăng Nhập',
-        href: `/user/setting/login-history`,
+        href: `/setting/login-history`,
       },
 
       {
         key: 'update-name',
         label: 'Cập Nhật Tên',
-        href: `/user/setting/update`,
+        href: `/setting/update`,
       },
     ],
   },
