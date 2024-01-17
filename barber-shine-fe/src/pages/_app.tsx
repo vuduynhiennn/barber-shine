17480685129c9c591e9521b2d81a51418478c5ac
nhienduyvu <nhienduyvu@gmail.com>
2024-01-17 15:02:06 +0700

    feat: get layout preserve state nextjs

--- barber-shine-fe/src/pages/_app.tsx
+++ barber-shine-fe/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import type { AppProps } from "next/app";
-import "../../app/globals.css";
+import type { AppProps } from 'next/app'
+import '../../app/globals.css'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const getLayout = Component.getLayout || ((page) => page)
+  return getLayout(<Component {...pageProps} />)
 }
