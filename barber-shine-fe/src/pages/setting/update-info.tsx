3b0fff7a8758683608130be2161e060730fb998f
nhienduyvu <nhienduyvu@gmail.com>
2024-01-17 15:03:22 +0700

    chore: updateInfo page

--- barber-shine-fe/src/pages/setting/update-info.tsx
+++ barber-shine-fe/src/pages/setting/update-info.tsx
@@ -1,5 +1,21 @@
-import React from 'react'
+import DashboardLayout from '@/components/dashboard/dashboard-layout'
+import DevelopingWarn from '@/components/ui/developing'
+import LandingLayout from '@/layout/LandingLayout'
+import Wrapper from '@/layout/Wrapper'
+import React, { ReactElement } from 'react'
 
 export default function UpdateInfoPage() {
-  return <div>UpdateInfoPage</div>
+  return (
+    <DashboardLayout>
+      UpdateInfoPage <DevelopingWarn />
+    </DashboardLayout>
+  )
+}
+
+UpdateInfoPage.getLayout = function getLayout(page: ReactElement) {
+  return (
+    <LandingLayout>
+      <Wrapper className="md:py-3.5">{page}</Wrapper>
+    </LandingLayout>
+  )
 }
